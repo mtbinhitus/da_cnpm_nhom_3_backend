5575d1128ee55e51f568c21928e068c49bd4c671
khanh.dinhtrong <19127437@student.hcmus.edu.vn>
2023-03-17 01:20:17 +0700

    Add configuration fix ( Access-Control-Allow-Origin )

--- src/main/java/com/example/examservice/config/Configuration.java
+++ src/main/java/com/example/examservice/config/Configuration.java
@@ -1,4 +1,18 @@
 package com.example.examservice.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@org.springframework.context.annotation.Configuration
 public class Configuration {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("*").allowedOrigins("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 }
