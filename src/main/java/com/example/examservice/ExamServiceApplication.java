6e34ac7ec82df5917b8ddaa3380b7532e320d9a0
khanh.dinhtrong <19127437@student.hcmus.edu.vn>
2023-03-17 11:55:20 +0700

    no merge please

--- src/main/java/com/example/examservice/ExamServiceApplication.java
+++ src/main/java/com/example/examservice/ExamServiceApplication.java
@@ -1,10 +1,8 @@
 package com.example.examservice;
 
-import org.keycloak.adapters.KeycloakConfigResolver;
-import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ExamServiceApplication {
@@ -12,8 +10,5 @@ public class ExamServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ExamServiceApplication.class, args);
 	}
-	@Bean
-	public KeycloakConfigResolver keycloakConfigResolver() {
-		return new KeycloakSpringBootConfigResolver();
-	}
+
 }
